6378afea72910731385b0013ce72e9f8863044e5
gabriel.stoychev <gabriel.stoychev@CAB-21-04>
2023-03-05 16:04:03 +0200

    Add project files.

---
+++ CRUDUsingDBCommands/Program.cs
@@ -0,0 +1,19 @@
+﻿using CRUDUsingDBCommands.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDUsingDBCommands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display d = new Display();
+            //sss
+            int b = 5;
+        }
+    }
+}
